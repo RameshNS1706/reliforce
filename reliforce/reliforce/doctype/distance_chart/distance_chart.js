75347a66b470c675056d6f82d448a9a7e680395f
Rameshns1706 <rameshns1706@gmail.com>
2025-07-10 09:30:10 +0000

    New Doctypes

---
+++ reliforce/reliforce/doctype/distance_chart/distance_chart.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Ramesh N S and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Distance Chart", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ reliforce/reliforce/doctype/leads/leads.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Ramesh N S and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Leads", {
+// 	refresh(frm) {
+
+// 	},
+// });
