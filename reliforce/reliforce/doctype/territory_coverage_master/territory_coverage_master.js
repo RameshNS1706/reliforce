9d9010ae65d39bc173dd39e738c55ad49600f42d
Rameshns1706 <rameshns1706@gmail.com>
2025-07-16 11:25:23 +0000

    mom entry corrections

--- reliforce/reliforce/doctype/territory_coverage_master/territory_coverage_master.js
+++ reliforce/reliforce/doctype/territory_coverage_master/territory_coverage_master.js
@@ -6,3 +6,32 @@
 
 // 	},
 // });
+
+//get parent table employee data to child table
+frappe.ui.form.on("Territory Coverage Detail", {
+    territory: function(frm, cdt, cdn) {
+         var d = locals[cdt][cdn];
+         frappe.model.set_value(cdt, cdn,"employee",frm.doc.employee);}
+       });
+       
+//validate employee details in parent & chiild table
+frappe.ui.form.on("Territory Coverage Master", "validate", function(frm, cdt, cdn) { 
+	$.each(frm.doc.territory_detail, function(i, d) {
+		if(d.employee != frm.doc.employee) {
+			frappe.throw("Employee in Territory Details Table not matching with Sales Person.!");
+		}
+	});
+});
+
+//validate duplicates in child table before save
+frappe.ui.form.on("Territory Coverage Master", "validate", function(frm, cdt, cdn) {
+    var d = locals[cdt][cdn];
+    $.each(frm.doc.territory_detail || [], function(i, d) {
+    var count = 0;
+    cur_frm.doc.territory_detail.forEach(function(item){
+    count += item.territory === d.territory ? 1 : 0;    
+    if( d.idx + count > d.idx + 1 ){
+    frappe.throw("Duplicate Territory in Row! - "+ d.idx);
+        }});
+    });
+});
